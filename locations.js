58be06705c2cc6e469f18aa4f67aee1d6c11d69c
Andrew Huschka <huschkaa@gmail.com>
2018-02-18 22:19:34 -0600

    updated the locations with new ones

--- locations.js
+++ locations.js
@@ -27,5 +27,41 @@ var locations = [
     {
         title: 'King\'s BierHaus',
         location: {lat: 29.804320, lng: -95.427390}
-    }
-  ];
+    },
+    {
+        title: 'Heights Bier Garten',
+        location: {lat: 29.798742, lng: -95.410300}
+    },
+    {
+        title: 'Snooze',
+        location: {lat: 29.801749, lng: -95.410389}
+    },
+    {
+        title: 'Kraftsmen Baking',
+        location: {lat: 29.806291 , lng: -95.408453}
+    },
+    {
+        title: 'Torchy\'s Tacos',
+        location: {lat: 29.802756, lng: -95.403432}
+    },
+    {
+        title: 'Ritual',
+        location: {lat: 29.782037, lng: -95.387343}
+    },
+    {
+        title: 'Coltivare',
+        location: {lat: 29.781937, lng: -95.393853}
+    },
+    {
+        title: 'Fat Cat Creamery',
+        location: {lat: 29.803503, lng: -95.410771}
+    },
+    {
+        title: 'Ka Sushi',
+        location: {lat: 29.803214, lng: -95.410542}
+    },
+    {
+        title: 'Boomtown Coffee',
+        location: {lat: 29.802812, lng: -95.400814}
+    },
+];
